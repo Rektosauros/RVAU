1bfd09f7e692716514e9254d73c2ca09d8bc1741
Francisco Pinho <up201303744@fe.up.pt>
2018-12-30 21:04:50 +0000

    camera feed working

--- Augmentation.py
+++ Augmentation.py
@@ -2,13 +2,37 @@ import cv2
 import numpy as np
 import pickle
 from tkinter.filedialog import askopenfilename
+import tkinter
 from ImageData import ImageData
 
-filename = askopenfilename(title = "Select Preparation Data")
-with open(filename, 'rb') as f:
-    imagedata = pickle.load(f)
-    f.close()
-print(imagedata.kp)
-print(imagedata.desc)
-for ipoint in imagedata.ipoints:
-    print(ipoint)
+
+def getData():
+    root = tkinter.Tk()
+    root.withdraw()
+    filename = askopenfilename(title = "Select Preparation Data")
+    with open(filename, 'rb') as f:
+        imagedata = pickle.load(f)
+        f.close()
+    print(imagedata.kp)
+    print(imagedata.desc)
+    for ipoint in imagedata.ipoints:
+        print(ipoint)
+
+def captureVideo():
+    cap = cv2.VideoCapture(0)
+    while(True):
+        ret, frame = cap.read()
+        if ret:
+            # Our operations on the frame come here
+            cv2.imshow('AR Detection',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+def main():
+    getData()
+    captureVideo()
+
+if __name__ == '__main__':
+    main()  
